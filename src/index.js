8046a5e6425f9d6c1b23736472053136e8f995ff
aaaasmirnova <smirnovnana@yandex.ru>
2024-12-10 07:08:59 +0300

    Добавлен импорт css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import './styles.css'
+
 function addNumbers() {
     const num1 = parseFloat(document.getElementById("number1").value);
     const num2 = parseFloat(document.getElementById("number2").value);
     const result = num1 + num2;
     document.getElementById("result").innerText = `Result: ${result}`;
-  }
-  
-  document.getElementById("calculate").addEventListener("click", addNumbers);
+}
+
+document.getElementById("calculate").addEventListener("click", addNumbers);
