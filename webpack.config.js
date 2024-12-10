d7b4f9dd5b255babf0999f2f3899616af7cf6f16
aaaasmirnova <smirnovnana@yandex.ru>
2024-12-10 07:08:49 +0300

    Добавлен html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,41 @@ const path = require('path'); // Импортируем модуль "path" дл
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Точка входа для сборки проекта
+    entry: './src/index.js', // Точка входа для сборки проекта
+    mode: 'development', // Режим сборки
 
-  output: {
-    filename: './dict/bundle.js', // Имя выходного файла сборки
-    path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
-        use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
-      },
-    ],
-  },
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    output: {
+        filename: './dict/bundle.js', // Имя выходного файла сборки
+        path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
     },
-    open: true, // Автоматически открывать браузер
-  },
 
-  mode: 'development', // Режим сборки
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+                use: ['style-loader', 'css-loader'],
+            }, // Загрузчики, используемые для обработки CSS-файлов
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/,
+                type: 'asset/resource'
+            }
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'), // Каталог для статики
+        },
+        open: true, // Автоматически открывать браузер
+    },
 };
