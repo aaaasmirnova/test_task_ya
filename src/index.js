c7d6c308c26ee9c3ef206eec724dc0ca117cf9ba
aaaasmirnova <smirnovnana@yandex.ru>
2024-12-12 23:11:23 +0300

    Добавляет слайдер

--- src/index.js
+++ src/index.js
@@ -1,10 +1,75 @@
 import './styles.css'
 
-function addNumbers() {
-    const num1 = parseFloat(document.getElementById("number1").value);
-    const num2 = parseFloat(document.getElementById("number2").value);
-    const result = num1 + num2;
-    document.getElementById("result").innerText = `Result: ${result}`;
+// function addNumbers() {
+//     const num1 = parseFloat(document.getElementById("number1").value);
+//     const num2 = parseFloat(document.getElementById("number2").value);
+//     const result = num1 + num2;
+//     document.getElementById("result").innerText = `Result: ${result}`;
+// }
+//
+// document.getElementById("calculate").addEventListener("click", addNumbers);
+
+const stagesSliderButtonPrev = document.querySelector(".stages-slider__button-prev");
+const stagesSliderButtonNext = document.querySelector(".stages-slider__button-next");
+const stagesList = document.querySelector(".stages__list");
+const stagesSliderDot = document.querySelectorAll(".stages-slider__dot");
+const stagesItems = document.querySelectorAll(".stages__item");
+const slider = document.querySelector(".stages-slider");
+
+let sliderCount = 0;
+let sliderWidth;
+
+//адаптивность слайдера
+window.addEventListener('resize', showSlide);
+
+//Кнопки перелистывания слайдов вперед и назад
+stagesSliderButtonNext.addEventListener('click', nextSlide);
+stagesSliderButtonPrev.addEventListener('click', prevSlide);
+
+//Задает нужную ширину картинки и sliderLine
+function showSlide() {
+    sliderWidth = slider.offsetWidth;
+    stagesList.style.width = sliderWidth * stagesItems.length;
+    stagesList.style.transform = `translateX()`
+    stagesItems.forEach(item => item.style.width = sliderWidth);
+}
+showSlide();
+
+//Перелистывает слайд вперед
+function nextSlide() {
+    sliderCount++;
+    if (sliderCount >= stagesItems.length) sliderCount = 0;
+
+    rollSlider();
+    thisSlide(sliderCount);
 }
 
-document.getElementById("calculate").addEventListener("click", addNumbers);
+//Перелистывает слайд назад
+function prevSlide() {
+    sliderCount--;
+    if (sliderCount < 0) sliderCount = stagesItems.length - 1;
+
+    rollSlider();
+    thisSlide(sliderCount);
+}
+
+//задает шаг перемещения слайдов
+function rollSlider() {
+    stagesList.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+//Указывает как слайд по счету активен
+function thisSlide(index) {
+    stagesSliderDot.forEach(item => item.classList.remove('stages-slider__dot--active'));
+    stagesSliderDot[index].classList.add('stages-slider__dot--active');
+}
+
+//Вешаем клик на dot
+
+stagesSliderDot.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        sliderCount = index;
+        rollSlider();
+        thisSlide(sliderCount);
+    })
+})
