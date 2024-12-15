b4f406f4e414337bdbdbb9b2b484466f7fd4c3ae
aaaasmirnova <smirnovnana@yandex.ru>
2024-12-15 20:56:57 +0300

    Исправлен пути до файлов

--- dist/bundle.js
+++ dist/bundle.js
@@ -9,13 +9,11 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
+/***/ "../index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
 /***/ (() => {
-
-eval("function addNumbers() {\n    const num1 = parseFloat(document.getElementById(\"number1\").value);\n    const num2 = parseFloat(document.getElementById(\"number2\").value);\n    const result = num1 + num2;\n    document.getElementById(\"result\").innerText = `Result: ${result}`;\n  }\n  \n  document.getElementById(\"calculate\").addEventListener(\"click\", addNumbers);\n\n//# sourceURL=webpack://test_task_crowd/./src/index.js?");
 
 /***/ })
 
@@ -26,7 +24,7 @@ eval("function addNumbers() {\n    const num1 = parseFloat(document.getElementBy
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["../index.js"]();
 /******/ 	
 /******/ })()
 ;
