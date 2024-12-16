05852f55d581684573d34eb2b92434a1a3b796e3
aaaasmirnova <smirnovnana@yandex.ru>
2024-12-16 21:34:38 +0300

    Добавляет слайдер для блока с участниками

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const membersList = document.querySelector(".members__list");
 const membersItems = document.querySelectorAll(".members__item");
 const slides = Array.from(membersItems);
 const slider = document.querySelector(".members-slider");
+const memberCurrentSlide = document.querySelector('.members-slider__page-count-current');
+const memberTotalSlide = document.querySelector('.members-slider__page-count-total');
 
 let slideIndex = 0;
 const slideCount = slides.length;
@@ -18,12 +20,14 @@ buttonNext.addEventListener('click',showNextSlide);
 // Функция для показа предыдущего слайда
 function showPreviousSlide() {
     slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    memberCurrentSlide.textContent = slideIndex + 1;
     updateSlider();
 }
 
 // Функция для показа следующего слайда
 function showNextSlide() {
     slideIndex = (slideIndex + 1) % slideCount;
+    memberCurrentSlide.textContent = slideIndex + 1;
     updateSlider();
 }
 
